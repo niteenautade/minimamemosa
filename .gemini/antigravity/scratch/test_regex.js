18211a83fd8a8a4d8e2827f63fcbf7de15eae87b
Niteen Autade <niteenautade@gmail.com>
2026-07-16 11:10:24 +0530

    removed unnecessary test-*, .gemini files

--- .gemini/antigravity/scratch/test_regex.js
+++
@@ -1,4 +0,0 @@
-const html = "<p>Line 1</p><p>&nbsp;</p><p>Line 2</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>Line 3</p>";
-const updated1 = html.replace(/<p([^>]*)>(\s*<br[^>]*\/?>)+\s*<\/p>/gi, '<p$1>MMEMPTY</p>');
-const updated2 = updated1.replace(/<p([^>]*)>(?:\s|&nbsp;)*<\/p>/gi, '<p$1>MMEMPTY</p>');
-console.log(updated2);
--- .gemini/antigravity/scratch/test_shift_enter.js
+++
@@ -1,5 +0,0 @@
-const html = "<p>Line 1<br><br><br>Line 2</p>";
-const TurndownService = require('turndown');
-const ts = new TurndownService({ headingStyle: 'atx' });
-const md = ts.turndown(html);
-console.log(JSON.stringify(md));
